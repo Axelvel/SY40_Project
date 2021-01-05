e0d4a680f487fc7e28ce7212706f84017c84635b
Axelvel <49617085+Axelvel@users.noreply.github.com>
2021-01-06 07:06:58 +0900

    :sparkles: Updated Threads

--- Threads.c
+++ Threads.c
@@ -12,54 +12,88 @@
 
 //pthread_t tid[NbTh+1]; //
 pthread_mutex_t mutex;
-pthread_cond_t cbocal, cvalve, cclock, attendre;
+pthread_cond_t cbocal, cvalve, cclock, attendre, nouveau;
 
-pthread_t tbocal;
+pthread_t tbocal[10];
 pthread_t tvalve;
 pthread_t thorloge;
 
 
 void * bocal() {
     pthread_mutex_lock(&mutex);
+    
+    pthread_cond_wait(&nouveau, &mutex);
+    
     printf("Placer un bocal\n");
     
-	//pthread_cond_signal(&cbocal); //
-   
-	//sleep(1); //Marche avec le sleep
+	pthread_cond_signal(&cbocal);
+  
     
     pthread_cond_wait(&cvalve, &mutex);
 
-    printf("Enlever bocal\n");
+    printf("Enlever bocal\n\n");
+    
+    pthread_cond_signal(&nouveau);
     
     pthread_mutex_unlock(&mutex);
-    pthread_exit(NULL);
+    //pthread_exit(NULL);
+
+}
+
+void * bocal2() {
+    pthread_mutex_lock(&mutex);
+    
+    //pthread_cond_wait(&nouveau, &mutex);
+    
+    printf("Placer un bocal\n");
+    
+	pthread_cond_signal(&cbocal);
+  
+    
+    pthread_cond_wait(&cvalve, &mutex);
+
+    printf("Enlever bocal\n\n");
+    
+    //pthread_cond_signal(&nouveau);
+    
+    pthread_mutex_unlock(&mutex);
+    //pthread_exit(NULL);
 
 }
 
 void * valve() {
     pthread_mutex_lock(&mutex);
    
+	while(1) {
 		
-	//pthread_cond_wait(&cbocal, &mutex);
+	pthread_cond_wait(&cbocal, &mutex);
 
     printf("Ouverture valve\n");
     
+    for (int i = 0; i < 2;i++){
+    //printf("A ");
     pthread_cond_signal(&cclock);
     
     pthread_cond_wait(&attendre, &mutex);
-
+    //printf("B ");
+	}
     printf("Fermeture valve\n");
     pthread_cond_signal(&cvalve); 
+		
+	}
+	
     
     pthread_mutex_unlock(&mutex);
-    pthread_exit(NULL);
+    //pthread_exit(NULL);
 
 }
 
 
 void * clockt() {
     pthread_mutex_unlock(&mutex);
-
+    
+	while(1) {
+		
 	pthread_cond_wait(&cclock, &mutex);
     
     printf("Horloge lancée\n");
@@ -67,31 +101,58 @@ void * clockt() {
     printf("Horloge finie\n");
     pthread_cond_signal(&attendre);
     
+	}
 
     pthread_mutex_unlock(&mutex);
-    pthread_exit(NULL);
+    //pthread_exit(NULL);
 }
 
 
 int main(int argc, char const *argv[]) {
 
-  pthread_mutex_init(&mutex, NULL); //Initialisation du mutex
-
-  for (int j = 0; j < N; j++) {
+  //pthread_mutex_init(&mutex, NULL); //Initialisation du mutex
+  
+	if (pthread_mutex_init(&mutex, NULL) != 0) {
+		printf("mutex failed");
+	}else {
+		printf("mutex initialized!");
+	}
+  //for (int j = 0; j < N; j++) {
     printf("\n");
-    printf("Bocal : %i\n", j);
+   // printf("Bocal : %i\n", j);
 
-    pthread_create(&tbocal, NULL, bocal, NULL);
-    pthread_create(&tvalve, NULL, valve, NULL);
+    
+   
     pthread_create(&thorloge, NULL, clockt, NULL);
+    pthread_create(&tvalve, NULL, valve, NULL);
+    //sleep(1);
+   // for (int i = 0; i < 3; i++ ) {
+		 pthread_create(&tbocal[0], NULL, bocal, NULL);
+		 //sleep(1);
+		 pthread_create(&tbocal[1], NULL, bocal, NULL);
+		 
+		 pthread_create(&tbocal[2], NULL, bocal, NULL);
+		 //sleep(1);
+		 pthread_create(&tbocal[3], NULL, bocal, NULL);
+		 
+		 
+		 sleep(1);
+		 pthread_cond_signal(&nouveau);
+   // }
+    //pthread_exit(NULL);
 
-    pthread_join(tbocal, NULL);
+    /*for (int i = 0; i < 3; i++ )*/ pthread_join(tbocal[0], NULL);
     //pthread_join(tvalve, NULL);
     //pthread_join(thorloge, NULL);
- 
+	pthread_join(tbocal[1], NULL);
+	
+	pthread_join(tbocal[2], NULL);
+	
+	pthread_join(tbocal[3], NULL);
 
-    sleep(3);
-  }
+    //sleep(3);
+ // }
+  
   
   pthread_mutex_destroy(&mutex);
 
@@ -99,4 +160,5 @@ int main(int argc, char const *argv[]) {
 
 
   return 0;
+  //pthread_exit(NULL);
 }
