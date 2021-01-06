b20a6f23f26352d61e4f2ac308fba7a0ee5847d2
Axelvel <49617085+Axelvel@users.noreply.github.com>
2021-01-06 23:22:26 +0100

    :sparkles: Updated semaphores

--- semaphores.c
+++ semaphores.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,55 +7,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
+#include "utils.h"
 
-#define IFLAGS (SEMPERM | IPC_CREAT)
+
 #define SKEY   (key_t) IPC_PRIVATE //CLE CREE AU HASARD
 
-#define SEMPERM 0600		 /* Permission */
-
-
-/*********************************************************************/
-/*  Pour Operation P et V 					     */
-
 int sem_id ; //identificateur des sémaphores
-struct sembuf sem_oper_P ;  /* Operation P */
-struct sembuf sem_oper_V ;  /* Operation V */
 
-/*********************************************************************/
-
-int initsem(key_t semkey){
-
-	int status = 0;
-   	union semun {
-		int val;
-		struct semid_ds *stat;
-		short * array;
-	} ctl_arg;
-    if ((sem_id = semget(semkey, 6, IFLAGS)) > 0) {//Création de 5 sémaphores
-
-	    	short array[6] = {0, 0, 0, 0, 0, 0}; //initialisation des sémaphores à 0
-	    	ctl_arg.array = array;
-	    	status = semctl(sem_id, 0, SETALL, ctl_arg); //Création du tableau de sémaphore
-    }
-   if (sem_id == -1 || status == -1) {
-	perror("Erreur initsem");
-	return (-1);
-    } else return (sem_id);
-}
-
-void P(int semnum) { //Wait
-	sem_oper_P.sem_num = semnum; //numero du semaphore
-	sem_oper_P.sem_op  = -1 ; //valeur pour modifier le sempaphore
-	sem_oper_P.sem_flg = 0 ; //sem_undo ?
-	semop(sem_id,&sem_oper_P,1); //effectue l'opération sur le sémaphore
-}
-
-void V(int semnum) { //Wake
-	sem_oper_V.sem_num = semnum;
-	sem_oper_V.sem_op  = 1 ;
-	sem_oper_V.sem_flg  = 0 ;
-	semop(sem_id,&sem_oper_V,1);
-}
 
 
 void bocal(int n, int goal){
@@ -66,11 +25,12 @@ void bocal(int n, int goal){
           
           P(5);
 
-          printf("\nNouveau bocal %d\n", count);
+          printf("\nNouveau bocal %d (Type %d)\n", count, n);
             
           
           printf("Placer un bocal\n");
           V(1);
+          
         
           semctl(sem_id, 0, SETVAL, n);
 
@@ -99,7 +59,7 @@ void valve() {
           printf("Ouverture valve\n");
             
             int value = semctl(sem_id, 0, GETVAL);
-            printf("Type = %i\n", value);
+            //printf("Type = %i\n", m);
             
             for (int i = 0; i < value; i ++) {
                 V(2);
@@ -138,37 +98,50 @@ int main(int argc, char const *argv[]) {
   int i,semid; //Semid : identificateur des sémaphores
     initsem(SKEY); //On initialise le tableau de sémaphores
     
-    int a;
-    int b;
+    
+    int numberOfMachines;
+    int type;
+    int num;
+    int time;
     
     do {
-        printf("Enter the number of bocals (type 1): ");
+        printf("\nHow many production machines do you want?: ");
         fflush(stdin);
-    } while (scanf("%d", &a) != 1 || a <= 0);
+    } while (scanf("%d", &numberOfMachines) != 1 || numberOfMachines <= 0);
+    
+    
+    for (int i = 0; i < numberOfMachines; i++) {
+        
+        printf("\nMachine %d\n", i);
+        
+        do {
+            printf("What type: ");
+            fflush(stdin);
+        } while (scanf("%d", &type) != 1 || type <= 0);
+        
+        do {
+            printf("How many: ");
+            fflush(stdin);
+        } while (scanf("%d", &num) != 1 || num <= 0);
+        
+        bocal(type, num);
+    }
     
     do {
-        printf("Enter the number of bocals (type 2): ");
+        printf("\nHow long should the timer be?: ");
         fflush(stdin);
-    } while (scanf("%d", &b) != 1 || b <= 0);
+    } while (scanf("%d", &time) != 1 || time <= 0);
     
-      
-      
-      
 
-    bocal(1, a); //Machine 1, with 'a' bocals (type 1) to poduce
-    bocal(2, b); //Machine 2, with 'b' bocals (type 2) to produce
       
-      V(5); //Launch a machine
-      
-      valve();
-      horloge(1);
+    valve();
+    horloge(time);
+    
+    startProduction(0);
 
-      for (i=1; i<=2; i++) wait(0); //Ends the 2 machines
+      for (i=1; i<=numberOfMachines; i++) wait(0); //Ends the machines
       
       sleep(1);
-    
-    
-    
 
   //TODO: liberSem()
 
