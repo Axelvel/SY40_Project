9665b14c3c7cc15fb5072d1c2b0eb49b9fc57532
Axelvel <axelvel@live.fr>
2021-01-07 22:22:53 +0100

    :recycle: Refactor

--- semaphores.c
+++ semaphores.c
@@ -11,91 +11,106 @@
 
 
 #define SKEY   (key_t) IPC_PRIVATE //CLE CREE AU HASARD
-
 int sem_id ; //identificateur des sémaphores
 
 
-
 void bocal(int n, int goal){
-  if (!fork()) {
-      
-      int count = 0;
-      
-      while(count < goal){
-          
-          P(5);
-
-          printf("\nNouveau bocal %d (Type %d)\n", count, n);
-            
-          
-          printf("Placer un bocal\n");
-          V(1);
-          
-        
-          semctl(sem_id, 0, SETVAL, n);
-
-          P(4);
-          printf("Enlever bocal\n");
-            
-          V(5);
-          
-          sleep(2);
-          count++;
-      }
+    
+    
+    if (!fork()) {
         
       
+        int count = 0;
+      
+        while(count < goal) {
+          
+            P(5);
 
-    exit(0);
-  }
+            printf("\nNouveau bocal %d (Type %d)\n", count, n);
+            
+          
+            printf("Placer un bocal\n");
+          
+            V(1);
+          
+        
+            semctl(sem_id, 0, SETVAL, n);
+
+            P(4);
+          
+            printf("Enlever bocal\n");
+            
+            V(5);
+    
+            count++;
+        }
+
+        exit(0);
+      
+    }
+    
 }
 
 
 
 void valve() {
-  if (!fork()) {
     
-      while(1) {
-          P(1);
-          printf("Ouverture valve\n");
+    if (!fork()) {
+    
+        while(1) {
+            
+            P(1);
+            
+            printf("Ouverture valve\n");
             
             int value = semctl(sem_id, 0, GETVAL);
-            //printf("Type = %i\n", m);
             
             for (int i = 0; i < value; i ++) {
+                
                 V(2);
+                
                 P(3);
             }
           
-          printf("Fermeture valve\n");
+            printf("Fermeture valve\n");
             
-          V(4);
-      }
+            V(4);
+        }
 
-    exit(0);
-  }
+        exit(0);
+        
+    }
+    
 }
 
 
 void horloge(int i){
-  if (!fork()) {
-      while(1) {
-          P(2);
-          printf("Horloge lancée\n");
-          sleep(i);
-          printf("Horloge finie\n");
-          V(3);
-      }
+    
+    if (!fork()) {
+        
+        while(1) {
+            
+            P(2);
+            
+            printf("Horloge lancée\n");
+            
+            sleep(i);
+            
+            printf("Horloge finie\n");
+            
+            V(3);
+        }
     
 
-    exit(0);
-  }
+        exit(0);
+    }
 }
 
 
 
 int main(int argc, char const *argv[]) {
-
-  int i,semid; //Semid : identificateur des sémaphores
+    
+    int i,semid; //Semid : identificateur des sémaphores
     initsem(SKEY); //On initialise le tableau de sémaphores
     
     
@@ -105,8 +120,10 @@ int main(int argc, char const *argv[]) {
     int time;
     
     do {
+        
         printf("\nHow many production machines do you want?: ");
         fflush(stdin);
+        
     } while (scanf("%d", &numberOfMachines) != 1 || numberOfMachines <= 0);
     
     
@@ -115,21 +132,27 @@ int main(int argc, char const *argv[]) {
         printf("\nMachine %d\n", i);
         
         do {
-            printf("What type: ");
+            
+            printf("What type?: ");
             fflush(stdin);
+            
         } while (scanf("%d", &type) != 1 || type <= 0);
         
         do {
-            printf("How many: ");
+            
+            printf("How many?: ");
             fflush(stdin);
+            
         } while (scanf("%d", &num) != 1 || num <= 0);
         
         bocal(type, num);
     }
     
     do {
+        
         printf("\nHow long should the timer be?: ");
         fflush(stdin);
+        
     } while (scanf("%d", &time) != 1 || time <= 0);
     
 
@@ -139,11 +162,11 @@ int main(int argc, char const *argv[]) {
     
     startProduction(0);
 
-      for (i=1; i<=numberOfMachines; i++) wait(0); //Ends the machines
+    for (i=1; i<=numberOfMachines; i++) wait(0); //Ends the machines
       
-      sleep(1);
-
-  //TODO: liberSem()
+    sleep(1);
+    
+    printf("\nTerminé\n\n");
 
   return 0;
 }
