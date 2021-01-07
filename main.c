b66266d01e111a2b9edc0153de3875a691a10dcf
Axelvel <49617085+Axelvel@users.noreply.github.com>
2021-01-08 06:44:37 +0900

    :recycle: Refactor

--- main.c
+++ main.c
@@ -9,18 +9,22 @@ int main(int argc, char const *argv[]) {
     int choice;
     
     do {
+        
         printf("Quel programme voulez vous lancer ?\n");
         printf("    - Semaphores : 1\n");
         printf("    - Threads : 2\n");
         fflush(stdin);
+        
     } while (scanf("%d", &choice) != 1 || choice <= 0 || choice > 2);
     
-    if (choice == 1) {
-        //Semaphores
-        execl("semaphores", "semaphores", NULL);
-    } else if (choice == 2){
-        //Threads
-        execl("threads", "threads", NULL);
+    if (choice == 1) { //Semaphores
+        
+        execl("semaphores", "semaphores", NULL); //Execute le programme semaphores
+        
+    } else if (choice == 2){ //Threads
+        
+        execl("threads", "threads", NULL); //Execute le programme threads
+        
     }
 
   return 0;
